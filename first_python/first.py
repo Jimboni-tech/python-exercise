b618961e6dc9c6e51d0e618dcae8bec70ff1536a
Jimmy Zhou <yunchengzhou@gmail.com>
2020-11-29 21:41:07 -0600

    Added main

--- first_python/first.py
+++ first_python/first.py
@@ -1,4 +1,6 @@
-while True:
+
+def main():
+  while True:
 
     try:
         num = input('what is your favorite number? ')
@@ -7,6 +9,10 @@ while True:
         break
     except:
         print('Please enter a valid number')
+
+if __name__ == "__main__":
+  main()
+
 '''
 I learned how to use a while True loop in order to continously repeat until a certain statement was found, in this case, a number. I then learned how to use break to stop a loop.
 I also learned how to use try and except.
