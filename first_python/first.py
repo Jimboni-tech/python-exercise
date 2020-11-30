3e7d0679a7d82ed2954ff29440359f9fa9c4e8dd
root <root@DESKTOP-296PLAR.localdomain>
2020-11-29 21:35:58 -0600

    my first python

---
+++ first_python/first.py
@@ -0,0 +1,13 @@
+while True:
+
+    try:
+        num = input('what is your favorite number? ')
+        square = int(num)**2
+        print('The square of', num, 'is', square)
+        break
+    except:
+        print('Please enter a valid number')
+'''
+I learned how to use a while True loop in order to continously repeat until a certain statement was found, in this case, a number. I then learned how to use break to stop a loop.
+I also learned how to use try and except.
+'''
